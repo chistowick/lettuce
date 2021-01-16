73fe9377df590769bc302802eaeb38be037a38e9
Anatoly Chinyaev <chistowick@gmail.com>
2021-01-16 20:40:45 +0300

    Added: test testCanGetExchangeRatesForTheSelectedDate()

--- tests/ExchangeRatesHandlerTest.php
+++ tests/ExchangeRatesHandlerTest.php
@@ -20,7 +20,7 @@ final class ExchangeRatesHandlerTest extends TestCase
     /**
      * @dataProvider incorrectDatesProvider
      */
-    public function testCannotBeUsedWithIncorrectDateFormat(string $incorrect_date): void
+    public function testIfADateFormatIsIncorrectReturnsNull(string $incorrect_date): void
     {
         $erh = new ExchangeRatesHandler();
 
@@ -39,6 +39,26 @@ final class ExchangeRatesHandlerTest extends TestCase
             'a nonexistent day in a non-leap year' => ['2021-02-29'],
             'month lost' => ['2020--12'],
             'YYYY/MM/DD format' => ['2020/11/12']
+        ];
+    }
+
+    /**
+     * @dataProvider exchangeRatesProvider
+     */
+    public function testCanGetExchangeRatesForTheSelectedDate(string $date, string $currency_name, string $expected_rate)
+    {
+        $erh = new ExchangeRatesHandler();
+
+        $this->assertEquals($expected_rate, $erh->$currency_name($date));
+    }
+
+    public function exchangeRatesProvider(): array
+    {
+        return [
+            'HKD on 2021-01-01' => ['2021-01-01', 'HKD', '9.53013'],
+            'USD on 2021-01-01' => ['2021-01-01', 'USD', '73.8757'],
+            'HKD on 2011-01-04' => ['2011-01-04', 'HKD', ''],
+            'USD on 2011-01-04' => ['2011-01-04', 'USD', '30.3505'],
         ];
     }
 }
